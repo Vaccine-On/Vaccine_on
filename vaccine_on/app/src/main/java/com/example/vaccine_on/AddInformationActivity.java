246e305f742c2d5b5f0be1e8958d84894a22ee3c
noion0511 <noion0511@gmail.com>
2021-05-14 18:26:58 +0900

    프로젝트 개발 시작

---
+++ vaccine_on/app/src/main/java/com/example/vaccine_on/AddInformationActivity.java
@@ -0,0 +1,14 @@
+package com.example.vaccine_on;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddInformationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_information);
+    }
+}
