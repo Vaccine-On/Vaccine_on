6c1b9b54b6d3f3cceec25300007f83f30381bade
noion0511 <noion0511@gmail.com>
2021-05-17 17:13:14 +0900

    start

---
+++ vaccine_on/app/src/main/java/com/example/vaccine_on/StartActivity.java
@@ -0,0 +1,14 @@
+package com.example.vaccine_on;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+    }
+}
